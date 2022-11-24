9024ebda43804f756fd2fcf7a3520b5110a6f15a
Alwin <meijboom.am@gmail.com>
2022-11-24 21:59:52 +0100

    Add API endpoints; Create views; Update settings

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import GroupSerializer, AttributeSerializer, ItemSerializer
+from .models import Group, Attribute, Item
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by('-created')
+    serializer_class = GroupSerializer
+
+
+class AttributeViewSet(viewsets.ModelViewSet):
+    queryset = Attribute.objects.all().order_by('-created')
+    serializer_class = AttributeSerializer
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all().order_by('-created')
+    serializer_class = ItemSerializer
--- uims/settings.py
+++ uims/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+APPEND_SLASH = False
+
 
 # Application definition
 
@@ -41,7 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api"
+    "api",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
+}
