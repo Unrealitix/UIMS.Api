2c217b2d1009f04119da7a3b95fe85924fc62d65
Alwin <meijboom.am@gmail.com>
2022-11-24 21:57:37 +0100

    Add serializers; Update models

--- api/models.py
+++ api/models.py
@@ -29,7 +29,6 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, default=None,
                               blank=True, null=True)
-    attributes = models.ManyToManyField(Attribute, through='ItemAttribute')
     description = models.CharField(max_length=255, default=None, blank=True,
                                    null=True)
     sku = models.CharField(max_length=255)
@@ -51,7 +50,8 @@ class ItemAttribute(models.Model):
     attribute = models.ForeignKey(Attribute, on_delete=models.SET_NULL,
                                   default=None, blank=True, null=True)
     item = models.ForeignKey(Item, on_delete=models.SET_NULL,
-                             default=None, blank=True, null=True)
+                             default=None, blank=True, null=True,
+                             related_name="attributes")
     value = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- api/serializers.py
+++ api/serializers.py
@@ -1,28 +1,30 @@
 from rest_framework import serializers
 
-from .models import Group, Attribute, Item
+from .models import Group, Attribute, Item, ItemAttribute
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['name', 'created', 'updated']
 
 
-class AttributeSerializer(serializers.HyperlinkedModelSerializer):
+class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
         fields = ['name']
 
 
-class ItemSerializer(serializers.HyperlinkedModelSerializer):
+class ItemAttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemAttribute
+        fields = ['attribute', 'item', 'value', 'created', 'updated']
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    attributes = ItemAttributeSerializer(many=True, read_only=True)
+
     class Meta:
         model = Item
-        fields = ['name', 'group', 'attributes', 'description', 'sku',
+        fields = ['id', 'name', 'group', 'attributes', 'description', 'sku',
                   'barcode', 'quantity', 'supplier', 'created', 'updated']
-
-
-# class ItemAttributeSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = ItemAttribute
-#         fields = ['']
