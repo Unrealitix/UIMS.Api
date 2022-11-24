c829d115e7dccaaded07873b0008520591bffa11
Alwin <meijboom.am@gmail.com>
2022-11-24 16:15:25 +0100

    Add base models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "groups"
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "attributes"
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, default=None,
+                              blank=True, null=True)
+    attributes = models.ManyToManyField(Attribute)
+    description = models.CharField(max_length=255, default=None, blank=True,
+                                   null=True)
+    sku = models.CharField(max_length=255)
+    barcode = models.BigIntegerField(default=None, blank=True, null=True)
+    quantity = models.BigIntegerField()
+    supplier = models.CharField(max_length=255, default=None, blank=True,
+                                null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "items"
